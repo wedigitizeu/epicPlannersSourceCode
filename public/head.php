5f128e5f018ca0ea3cd673494ccb2a9d9a47bbbe
Prakash <prakashk827@gmail.com>
2020-04-10 00:39:40 +0530

    jQuery CDN Link Added

--- public/head.php
+++ public/head.php
@@ -18,6 +18,9 @@
 <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
 <link rel="icon" href="images/favicon.png" type="image/x-icon">
 
+<!-- jQuery CDN -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
 <!-- Responsive -->
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
