3275b684c6c4461810791b66cc1d3f4d1c42dd3c
Prakash <prakashk827@gmail.com>
2020-04-09 22:12:24 +0530

    Admin Folder with files added

---
+++ admin/properties/icons.php
@@ -0,0 +1,21 @@
+<?php
+
+function gallery()
+{
+	echo 'fa-picture-o';
+}
+function contactList()
+{
+	echo 'fa-list';
+}
+
+function pdf()
+{
+	echo 'fa-file-pdf-o';
+}
+
+function leadListIcon()
+{
+	echo 'fa-money';
+}
+?>
---
+++ admin/properties/labels.php
@@ -0,0 +1,41 @@
+<?php
+function  dateLabel()
+{
+	echo 'Date';
+}
+function  timeLabel()
+{
+	echo 'Time';
+}
+
+function  name()
+{
+	echo 'Name';
+}
+
+function email ()
+{
+	echo 'Email';
+}
+
+function mobile()
+{
+	echo 'Mobile';
+}
+
+function  looking()
+{
+	echo 'Looking for';
+}
+
+function  location()
+{
+	echo 'Location';
+}
+
+function  address()
+{
+	echo 'Address';
+}
+
+?>
