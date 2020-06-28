15d4a807711ab080c1adc5cf9d9c78c994757159
Sagar-code-14 <60517287+Sagar-code-14@users.noreply.github.com>
2020-06-28 16:10:08 +0530

    Changes Done
    Footer color changed
    Slider image changed
    Gallary Changed

--- public/counterSection.php
+++ public/counterSection.php
@@ -1,18 +1,3 @@
-<?php 
-include_once("public/head.php");
-/* For Displaying coffee with clients*/
-$query="SELECT * FROM `contact`";
-$exe=mysqli_query($conn,$query);
-$totalContacts=mysqli_num_rows($exe)+25;
-$query="SELECT * FROM `contact` WHERE status='lead'";
-$exe=mysqli_query($conn,$query);
-$totalLeads=mysqli_num_rows($exe)+15;
-
-if($totalContacts == 0)
-{
-	$totalContacts =$totalLeads;
-}
-?>
 <section class="counter-section style-two">
 		<div class="auto-container">
 		
@@ -37,7 +22,7 @@ if($totalContacts == 0)
 						<div class="inner wow fadeInLeft" data-wow-delay="300ms" data-wow-duration="1500ms">
 							<div class="content">
 								<div class="count-outer count-box alternate">
-									<span class="count-text" data-speed="3000" data-stop="<?php echo $totalContacts; ?>"></span>+
+									<span class="count-text" data-speed="3000" data-stop="30"></span>+
 								</div>
 								<h4 class="counter-title">Coffee with Client</h4>
 							</div>
@@ -49,21 +34,21 @@ if($totalContacts == 0)
 						<div class="inner wow fadeInLeft" data-wow-delay="600ms" data-wow-duration="1500ms">
 							<div class="content">
 								<div class="count-outer count-box">
-									<span class="count-text" data-speed="3000" data-stop="<?php echo $totalLeads; ?>">0</span>+
+									<span class="count-text" data-speed="3000" data-stop="10">0</span>+
 								</div>
-								<h4 class="counter-title">Successfull Events Delivered </h4>
+								<h4 class="counter-title">Team Members</h4>
 							</div>
 						</div>
 					</div>
 					
-					<!--Column-->
+					<!-- Column -->
 					<div class="column counter-column col-lg-3 col-md-6 col-sm-12">
 						<div class="inner wow fadeInLeft" data-wow-delay="900ms" data-wow-duration="1500ms">
 							<div class="content">
 								<div class="count-outer count-box">
-									<span class="count-text" data-speed="2500" data-stop="10">0</span>+
+									<span class="count-text" data-speed="2500" data-stop="8">0</span>
 								</div>
-								<h4 class="counter-title">Team Members</h4>
+								<h4 class="counter-title">Successfull Events Delivered</h4>
 							</div>
 						</div>
 					</div>
